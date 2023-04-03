cbfb1ba843d3884c154cb7a82bcfb430f37d0915
Nathan Joel Schofield <schofieldn@maths.ox.ac.uk>
2023-04-03 13:53:35 +0100

    Binary folder added

---
+++ heterogeneous_binary/cell_coagulation.m
@@ -0,0 +1,28 @@
+function coagulation = cell_coagulation(n,i,t,N)
+
+    function out = B_ij(i,j,t)
+        % Need very small b constant
+        b = 0.0001;
+        out = b*1;
+        % out = 1;
+    end
+
+% Coagulation term calculation
+    
+    % 1st sum of coagulation term calculation
+    sum1 = 0;
+    if i == 1
+        sum1 = 0;
+    else
+        for j = 1:i-1
+            sum1 = sum1 + B_ij(i,j,t)*n(i-j)*n(j);
+        end
+    end
+
+    % 2nd sum of coagulation term calculation
+    sum2 = 0;
+    for j = 1:N
+            sum2 = sum2 + B_ij(i,j,t)*n(i)*n(j);
+    end
+coagulation = (1/2)*sum1 - sum2;
+end
