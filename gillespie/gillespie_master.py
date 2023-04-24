f6352332cf3c2655c448f4c222d0590c3e43cf02
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-04-24 15:37:02 +0100

    Algorithm updated to before psi gets updated

--- gillespie/gillespie_master.py
+++ gillespie/gillespie_master.py
@@ -2,6 +2,7 @@
 import numpy as np
 
 import initial_conditions
+import step_functions as step_fns
 
 # Global constants
 N = 100 # The maximal cluster size
@@ -42,6 +43,22 @@ print(min(c_v))
 print(c_v[0])
 
 
-IC = initial_conditions.set_initial_conditions(N,3)
+IC = initial_conditions.set_initial_conditions(N,1)
 print(IC)
+print(sum(IC))
 
+t = 0
+simulation_counter = 0
+simulation_max = 1000
+
+psi_output = np.zeros(simulation_max, N+1)
+t_output = np.zeros(simulation_max)
+
+psi_output[0,:] = IC
+
+while simulation_counter < simulation_max:
+    psi_new = step_fns.single_step(c_v, psi_old, t, N)
+    print('t is: ', t)
+    psi_output[simulation_counter,:] = psi_new
+    psi_old = psi_new
+    simulation_counter += 1
