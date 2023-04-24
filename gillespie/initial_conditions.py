097a5e62912b084b9d539fb3a52edce3d52a7ed9
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-04-24 11:55:54 +0100

    exponential initial condition added

--- gillespie/initial_conditions.py
+++ gillespie/initial_conditions.py
@@ -34,17 +34,28 @@ def set_initial_conditions(N, opt):
     if opt == 3:
         fig, ax = plt.subplots(1, 1)
 
-        r = expon.rvs(size=1000000)
+    r = expon.rvs(size=1000)
 
-        x = np.linspace(expon.ppf(0.01),
+    x = np.linspace(expon.ppf(0.01),
                 expon.ppf(0.99), 100)
 
-        ax.hist(r, density=True, bins=100, histtype='stepfilled', alpha=0.2)
-        ax.set_xlim([x[0], x[-1]])
-        ax.legend(loc='best', frameon=False)
-        plt.show()
-        for i in range(100):
-            IC[i] = r[i]
+    ax.hist(r, density=True, bins=100, histtype='stepfilled', alpha=0.2)
+    ax.set_xlim([x[0], x[-1]])
+    ax.legend(loc='best', frameon=False)
+    plt.show()
+
+
+    # x = np.random.rand(100)
+    plt.hist(r)
+    # plt.show()
+    ax2 = plt.gca() # get axis handle
+
+    p = ax2.patches
+    p[0].get_height()
+
+    heights = [patch.get_height() for patch in p]
+    print(heights)
+    IC = heights
 
     return IC
 
--- gillespie/test_exp.py
+++ gillespie/test_exp.py
@@ -5,7 +5,7 @@ import numpy as np
 
 fig, ax = plt.subplots(1, 1)
 
-r = expon.rvs(size=1000000)
+r = expon.rvs(size=1000)
 
 x = np.linspace(expon.ppf(0.01),
                 expon.ppf(0.99), 100)
@@ -14,3 +14,15 @@ ax.hist(r, density=True, bins=100, histtype='stepfilled', alpha=0.2)
 ax.set_xlim([x[0], x[-1]])
 ax.legend(loc='best', frameon=False)
 plt.show()
+
+
+# x = np.random.rand(100)
+plt.hist(r)
+# plt.show()
+ax2 = plt.gca() # get axis handle
+
+p = ax2.patches
+p[0].get_height()
+
+heights = [patch.get_height() for patch in p]
+print(heights)
