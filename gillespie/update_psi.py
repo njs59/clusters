f3ffac8f71043f322b76ff6589c95fd4f92e4abc
Nathan Schofield <nathan.schofield@dtc.ox.ac.uk>
2023-04-24 16:10:02 +0100

    Mitosis and death update to psi

--- gillespie/update_psi.py
+++ gillespie/update_psi.py
@@ -8,3 +8,16 @@ def update_master(psi, index):
     psi: distribution to be updated
     index: Integer between 0 and 5196 for python subscript of corresponding equation
     '''
+    if index in range(0, 2500):
+        ## COagulation update
+        Stuff
+    elif index in range(2500, 2599):
+        ## Mitosis update
+        psi[index - 2500] -= 1 # Remove 1 cluster of size i
+        psi[index - 2500 + 1] += 1 # Add 1 cluster of size i+1
+    elif index in range(2599, 2698):
+        ## Death update
+        Stuff
+    elif index in range(2698, 5197):
+        ## Splitting update
+        Stuff
