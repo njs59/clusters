6c875d91a69420484534bef7c0b51bd6a0c4b273
Nathan Joel Schofield <schofieldn@maths.ox.ac.uk>
2023-11-28 17:24:14 +0000

    Hetero now prints output csv to correct directory

--- gillespie_hetero/initial_condition.py
+++ gillespie_hetero/initial_condition.py
@@ -4,30 +4,37 @@ import pandas as pd
 import select_event
 import event_update
 
+
+import os
+# Make sure we are in the correct working directory
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 ## SIngleton only initial condition
 
+# initial_cells_number = 200
+# inital_pro_prop = 0.5
+
+# array = np.full((initial_cells_number, 2), 1, dtype=int)
+
+# initial_pro = int(inital_pro_prop * initial_cells_number)
+# print(initial_pro)
+# for i in range(initial_pro):
+#     array[i,-1] = 0
+
+# print(array)
+# print(array.shape)
+# print(array.shape[0])
+
 initial_cells_number = 200
-inital_sus_prop = 0.5
-
-array = np.full((initial_cells_number, 2), 1, dtype=int)
-
-initial_sus = int(inital_sus_prop * initial_cells_number)
-print(initial_sus)
-for i in range(initial_sus):
-    array[i,-1] = 0
-
-print(array)
-print(array.shape)
-print(array.shape[0])
-
-initial_cells_number = 200
-inital_sus_prop = 0.5
+inital_pro_prop = 0.1
 
 array_test = np.full((initial_cells_number, 2), 1, dtype=int)
 
-initial_sus = int(inital_sus_prop * initial_cells_number)
-print(initial_sus)
-for i in range(initial_sus):
+initial_pro = int(inital_pro_prop * initial_cells_number)
+print(initial_pro)
+for i in range(initial_pro):
     array_test[i,-1] = 0
 
 data = ['Total cells', 'Proliferative cells', 'Timestep']
@@ -44,9 +51,10 @@ for l in range(1000):
     curr_shape = array_test.shape
     times = np.full((curr_shape[0], 1), l+1)
     data_step = np.append(array_test, times, axis=1)
-
+                                                                        
     data = np.vstack((data, data_step))
     # print('Current array', array_test)
 
 df = pd.DataFrame(data)
-df.to_csv('data.csv', index=False, header=False)
+df.to_csv('data2.csv', index=False, header=False)
+
