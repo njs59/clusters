489268a438e285d91d854ca3076533599a4d2a50
Nathan Joel Schofield <schofieldn@maths.ox.ac.uk>
2023-11-30 14:20:24 +0000

    Initial plotting code added

---
+++ gillespie_hetero/plots.py
@@ -0,0 +1,179 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy as sci
+
+import glob
+import contextlib
+from PIL import Image
+
+
+def final_step_plot(psi, simulation_max):
+    n_max = np.max(psi)
+    # plots_number = simulation_max
+    x = range(1,101)
+    y = psi[-1,:]
+    plt.bar(x,y)
+    plt.xlabel('Cluster size')
+    plt.ylabel('Average number of clusters')
+    plt.savefig("plots_to_gif/final_plot" + ".jpg")
+    plt.clf()
+
+def final_step_normalise_plot(psi, simulation_max):
+    n_max = np.max(psi)
+    # plots_number = simulation_max
+    x = range(1,101)
+    y_raw = psi[-1,:]
+    y_sum = np.sum(y_raw)
+    y = y_raw/y_sum
+    y_check = np.sum(y)
+    print('Check', y_check)
+    plt.bar(x,y)
+    plt.xlabel('Cluster size')
+    plt.ylabel('Normalised number of clusters')
+    plt.savefig("plots_to_gif/final_plot_normalised" + ".jpg")
+    plt.clf()            
+
+def final_step_mass_plot(psi, simulation_max):
+    n_max = np.max(psi)
+    # plots_number = simulation_max
+    x = range(1,101)
+    y_raw = psi[-1,:]
+    y_mass = np.zeros(100)
+    for i in range(100):
+        y_mass[i] = (i+1)*y_raw[i]
+    y_sum = np.sum(y_mass)
+    y = y_mass/y_sum
+    y_check = np.sum(y)
+    print('Check', y_check)
+    plt.bar(x,y)
+    plt.xlabel('Cluster size')
+    plt.ylabel('Chance cell is in cluster')
+    plt.savefig("plots_to_gif/final_plot_mass" + ".jpg")
+    plt.clf()   
+
+def final_step_mass_hist(psi, simulation_max, sim_num):
+    n_max = np.max(psi)
+    # plots_number = simulation_max
+    x = range(1,101,5)
+    y_raw = psi[-1,:]
+    print('y raw', y_raw)
+    y_raw_scale = sim_num*y_raw
+    hist_values = []
+    for k in range (100):
+        count = y_raw_scale[k]
+        count_round = round(count)
+        for l in range(count_round*(k+1)):
+            hist_values.append(k+1)
+            #print('l', l)
+
+    #y_mass = np.zeros(20)
+    #for j in range(20):
+    #    for i in range(5):
+    #        y_mass[j] = (5*j+i+1)*y_raw[5*j+i]
+    #y_sum = np.sum(y_mass)
+    #y = y_mass/y_sum
+    #y_check = np.sum(y)
+    #print('Check', y_check)
+    print('Hist values', hist_values)
+    plt.hist(hist_values, bins=20, density=True)
+    #plt.bar(x,y)
+    plt.xlabel('Cluster size')
+    plt.ylabel('Chance cell is in cluster')
+    plt.savefig("plots_to_gif/final_plot_mass_hist" + ".jpg")
+    iqr = sci.stats.iqr(hist_values)
+    print('iqr', iqr)
+    plt.clf()  
+
+
+def animate_plot(psi, t, simulation_max):
+    n_max = np.max(psi)
+    plots_number = simulation_max
+    for i in range(0,plots_number+1,1):
+        plots_step = math.floor(len(t)/plots_number)
+        x = range(1,101)
+        y = psi[i*plots_step,:]
+        plt.bar(x,y)
+        plt.ylim([0, n_max])
+        if i < 10:
+            print ("value" + str(0) + str(0) + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/value" + str(0) + str(0) + str(i) + ".jpg")
+            plt.clf()
+        elif 10 <= i < 100:
+            print ("value" + str(0) + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/value" + str(0) + str(i) + ".jpg")
+            plt.clf()
+        elif 100 <= i < 110:
+            print ("value" + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/value" + str(i) + ".jpg")
+            plt.clf()
+        else:
+            print ("value" + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/value" + str(i) + ".jpg")
+            plt.clf()
+
+        # filepaths
+        fp_in = "plots_to_gif/value*.jpg"
+        fp_out = "plots_to_gif/clustersize.gif"
+
+        # use exit stack to automatically close opened images
+        with contextlib.ExitStack() as stack:
+
+        # lazily load images
+            imgs = (stack.enter_context(Image.open(f))
+                for f in sorted(glob.glob(fp_in)))
+
+        # extract  first image from iterator
+            img = next(imgs)
+
+        # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+            img.save(fp=fp_out, format='GIF', append_images=imgs,
+                save_all=True, duration=200, loop=0)
+            
+
+
+def animate_plot_mass(psi, t, simulation_max):
+    n_max = np.max(psi)
+    plots_number = simulation_max
+    for i in range(0,plots_number+1,1):
+        plots_step = math.floor(len(t)/plots_number)
+        x = range(1,101)
+        y = psi[i*plots_step,:]
+        for z in range(1,101):
+            y[z-1] = z*y[z-1]
+        plt.bar(x,y)
+        plt.ylim([0, n_max])
+        if i < 10:
+            print ("mass_value" + str(0) + str(0) + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/mass_value" + str(0) + str(0) + str(i) + ".jpg")
+            plt.clf()
+        elif 10 <= i < 100:
+            print ("mass_value" + str(0) + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/mass_value" + str(0) + str(i) + ".jpg")
+            plt.clf()
+        elif 100 <= i < 110:
+            print ("mass_values" + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/mass_values" + str(i) + ".jpg")
+            plt.clf()
+        else:
+            print ("mass_values" + str(i) + ".jpg")
+            plt.savefig("plots_to_gif/mass_values" + str(i) + ".jpg")
+            plt.clf()
+
+        # filepaths
+        fp_in = "plots_to_gif/mass_value*.jpg"
+        fp_out = "plots_to_gif/clustersize_mass.gif"
+
+        # use exit stack to automatically close opened images
+        with contextlib.ExitStack() as stack:
+
+        # lazily load images
+            imgs = (stack.enter_context(Image.open(f))
+                for f in sorted(glob.glob(fp_in)))
+
+        # extract  first image from iterator
+            img = next(imgs)
+
+        # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
+            img.save(fp=fp_out, format='GIF', append_images=imgs,
+                save_all=True, duration=200, loop=0)
--- gillespie_hetero/sim_runner.py
+++ gillespie_hetero/sim_runner.py
@@ -1,17 +1,18 @@
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 import initial_condition
 import select_event
 import event_update
-
-
+import plots as gill_plt
 
 initial_cells_number = 200
 initial_pro_prop = 0.1
+num_steps = 1000
+
 
 init_array = initial_condition.initial_setup(initial_cells_number, initial_pro_prop)
-
 
 
 data = ['Total cells', 'Proliferative cells', 'Timestep']
@@ -21,16 +22,38 @@ data_step = np.append(init_array, times, axis=1)
 
 data = np.vstack((data, data_step))
 
-for l in range(1000):
-    event = select_event.event_selector(array_test)
-    array_test = event_update.event_selector(array_test, event)
+for l in range(num_steps):
+    event = select_event.event_selector(init_array)
+    init_array = event_update.event_selector(init_array, event)
 
-    curr_shape = array_test.shape
+    curr_shape = init_array.shape
     times = np.full((curr_shape[0], 1), l+1)
-    data_step = np.append(array_test, times, axis=1)
+    data_step = np.append(init_array, times, axis=1)
                                                                         
     data = np.vstack((data, data_step))
     # print('Current array', array_test)
 
 df = pd.DataFrame(data)
-df.to_csv('data2.csv', index=False, header=False)
+
+x = range(1,101)
+y = data_step[:,0]
+plt.hist(y,x)
+plt.xlabel('Cluster size')
+# plt.ylabel('Average number of clusters')
+# plt.savefig("plots_to_gif/final_plot" + ".jpg")
+plt.show()
+plt.clf()
+
+#df.to_csv('data2.csv', index=False, header=False)
+
+
+
+# # gill_plt.animate_plot_mass(psi_output, t_output, simulation_max)
+
+# gill_plt.final_step_plot(data, num_steps)
+
+# gill_plt.final_step_normalise_plot(data, num_steps)
+
+# gill_plt.final_step_mass_plot(data, num_steps)
+
+# # gill_plt.final_step_mass_hist(psi_output, num_steps, sim_num)
