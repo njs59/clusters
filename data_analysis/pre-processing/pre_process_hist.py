88e1812fcc565659495a009136dd3e01b6764e2e
Nathan Schofield <nathan@Ludwigs-Laptop.local>
2024-02-06 16:48:11 +0000

    pre-pro hist properly tidied and commented

--- data_analysis/pre-processing/pre_process_hist.py
+++ data_analysis/pre-processing/pre_process_hist.py
@@ -4,10 +4,11 @@ import pandas as pd
 from PIL import Image
 
 import matplotlib.animation as animation
-from IPython import display
 
 import pre_pro_operators as pre_oper
 
+
+###   -----------   Input parameters   ----------   ###
 basedir = '/Users/Nathan/Documents/Oxford/DPhil/'
 experiment = '2017-02-03_sphere_timelapse/'
 exp_date = '2017-02-03_'
@@ -20,6 +21,10 @@ time_array = range(31,98)
 time_list = [str(x).zfill(2) for x in time_array]
 well_loc = 's12'
 
+
+###   --------------   Plotting code   ---------   ###
+
+# Read in cluster areas
 cluster_2D_areas_csv_name_list = basedir, 'csv_folder/', exp_date, 'sphere_timelapse_', well_loc, '_cluster_areas', '.csv'
 cluster_2D_areas_csv_name_list_2  =''.join(cluster_2D_areas_csv_name_list)
 df_clus_areas = pd.read_csv(cluster_2D_areas_csv_name_list_2, header=None)
@@ -27,14 +32,16 @@ cluster_areas = df_clus_areas.to_numpy()
 
 number_of_frames = len(time_list)
 data = cluster_areas
-
+# Get correct row index to start on
 start_data = time_array[0] - 1
 
+# Plot histograms at each time
 data_times = data[start_data:,]
 fig = plt.figure()
 plt.axis([150,25000,0,200])
 hist = plt.hist(data_times[0], bins=[200, 600, 1000, 2000, 4000, 8000, 16000, 25000])
 
+# Plot as an animation object using the update_hist operator 
 animation = animation.FuncAnimation(fig, pre_oper.update_hist, number_of_frames, interval=500, fargs=(data_times, ) )
 
 
