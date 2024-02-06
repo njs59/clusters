c2721043bfc0afadf7543ed66a5914bac80ff8f5
Nathan Schofield <nathan@Ludwigs-Laptop.local>
2024-02-06 11:50:28 +0000

    Commented code deleted from post pro plots

--- data_analysis/post-processing/post_processing_plots.py
+++ data_analysis/post-processing/post_processing_plots.py
@@ -1,8 +1,3 @@
-# import numpy as np
-# import pandas as pd
-
-# import matplotlib.pyplot as plt
-
 import lineage_tracer_function as ltf
 import cluster_tracker_function as ctf
 
@@ -15,11 +10,3 @@ ctf.cluster_tracker(37, 97, 5, 10, basedir, exp_date, well_loc)
 
 
 ltf.lineage_tracer(51,97, basedir, exp_date, well_loc)
-
-
-    
-
-
-# def lineage_calc():
-#     list_of_clus = 0
-#     return list_of_clus
