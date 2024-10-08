f423d271ff908b2bcf78fcf2a1af54ccdccc7c06
Nathan Schofield <nathan@dhcp-163-1-81-23.maths.ox.ac.uk>
2024-10-08 17:48:06 +0100

    Cluster identification plots code updated

--- worked_example/data_analysis/cluster-identification/cluster_identification_plots.py
+++ worked_example/data_analysis/cluster-identification/cluster_identification_plots.py
@@ -13,70 +13,57 @@ from pylab import *
 from scipy.ndimage import *
 
 
-import pre_pro_operators as pre_oper
+import cluster_identification_operators as iden_oper
 
-
+# basedir, exp_type, 'pre_processing_output/', exp_date, '/', well_loc, '_cluster_areas', '.csv'
 ### ------------   Input parameters    -----------------  ###
-basedir = '/Users/Nathan/Documents/Oxford/DPhil/'
-exp_type = 'In_vitro_homogeneous_data/'
-experiment = 'RAW_data/2017-02-03_sphere_timelapse/'
-experiment = 'RAW_data/2017-02-13_sphere_timelapse_2/'
+basedir = '/Users/Nathan/Documents/Oxford/DPhil/clusters/worked_example/'
+csv_loc = 'data_analysis_outputs/csv_files/'
+saving_plot_loc = 'data_analysis_outputs/plots/'
+
 # exp_date = '2017-02-03'
-# exp_date = '2017-02-13'
-# exp_date = '2017-03-13'
-# exp_date = '2017-03-16'
-exp_date = '2017-03-24'
-folder = 'RAW/Timelapse/sphere_timelapse_useful_wells/'
-folder_3 = 'sphere_timelapse/'
-fileID = '.tif'
-
 # time_array = range(1,98)
-# time_array = range(1,95)
-time_array = range(1,146)
-# time_array = range(1,97)
-
-# Rename single digit values with 0 eg 1 to 01 for consistency
 # time_list = [str(x).zfill(2) for x in time_array]
-time_list = [str(x).zfill(3) for x in time_array]
-# time_list= ['21','22','23','24','25','26','27','28','29','30']
-
-
 # well_loc = 's11'
-# well_loc = 's12'
-# well_loc = 's27'
+
+exp_date = '2017-03-16'
+time_array = range(1,146)
+time_list = [str(x).zfill(3) for x in time_array]
 # well_loc = 's037'
-# well_loc = 's074'
-# well_loc = 's001'
-well_loc = 's04'
+well_loc = 's074'
 
 # Does the histogram get plotted
 plot_hist = True
+
+
+
+### -----------------  Plotting code  ----------------- ###
 
 num_clusters = []
 cluster_areas = np.array([])
 
 
 # Read in 2D array of cluster areas over time
-cluster_2D_areas_csv_name_list = basedir, exp_type, 'pre_processing_output/', exp_date, '/', well_loc, '_cluster_areas', '.csv'
+cluster_2D_areas_csv_name_list = basedir, csv_loc, exp_date, '/', well_loc, '_cluster_areas', '.csv'
 cluster_2D_areas_csv_name_list_2  =''.join(cluster_2D_areas_csv_name_list)
 df_clus_areas = pd.read_csv(cluster_2D_areas_csv_name_list_2, header=None)
 cluster_2D_areas = df_clus_areas.to_numpy()
 print('Shape', cluster_2D_areas.shape)
 
 # Read in mean area of cluster
-mean_areas_csv_name_list = basedir, exp_type, 'pre_processing_output/', exp_date, '/', well_loc, '_mean_areas', '.csv'
+mean_areas_csv_name_list = basedir, csv_loc, exp_date, '/', well_loc, '_mean_areas', '.csv'
 mean_areas_csv_name_list_2  =''.join(mean_areas_csv_name_list)
 df_mean_areas = pd.read_csv(mean_areas_csv_name_list_2, header=None)
 mean_areas = df_mean_areas.to_numpy()
 
 # Read in total cluster coverage area
-total_areas_csv_name_list = basedir, exp_type, 'pre_processing_output/', exp_date, '/', well_loc, '_total_areas', '.csv'
+total_areas_csv_name_list = basedir, csv_loc, exp_date, '/', well_loc, '_total_areas', '.csv'
 total_areas_csv_name_list_2  =''.join(total_areas_csv_name_list)
 df_total_areas = pd.read_csv(total_areas_csv_name_list_2, header=None)
 total_areas = df_total_areas.to_numpy()
 
 # Read in number of clusters
-number_clusters_csv_name_list = basedir, exp_type, 'pre_processing_output/', exp_date, '/', well_loc, '_number_clusters', '.csv'
+number_clusters_csv_name_list = basedir, csv_loc, exp_date, '/', well_loc, '_number_clusters', '.csv'
 number_clusters_csv_name_list_2  =''.join(number_clusters_csv_name_list)
 df_number_clusters = pd.read_csv(number_clusters_csv_name_list_2, header=None)
 number_clusters = df_number_clusters.to_numpy()
@@ -84,21 +71,21 @@ number_clusters = df_number_clusters.to_numpy()
 # Plot mean size of cluster
 plt.plot(mean_areas/189)
 plt.title("Mean Areas")
-plt.savefig(basedir + 'clusters/data_analysis/pre-processing/Mean_areas_' + well_loc + '.png', dpi=300)
+plt.savefig(basedir + saving_plot_loc + 'Mean_areas_' + well_loc + '.png', dpi=300)
 # plt.show()
 plt.clf()
 
 # Plot total coverage of clusters over array
 plt.plot(total_areas/189)
 plt.title("Total 2D area")
-plt.savefig(basedir + 'clusters/data_analysis/pre-processing/Total_2D_cell_area_' + well_loc + '.png', dpi=300)
+plt.savefig(basedir + saving_plot_loc + 'Total_2D_cell_area_' + well_loc + '.png', dpi=300)
 # plt.show()
 plt.clf()
 
 # Plot number of clusters
 plt.plot(number_clusters)
 plt.title("Number of clusters over time")
-plt.savefig(basedir + 'clusters/data_analysis/pre-processing/Number_clusters' + well_loc + '.png', dpi=300)
+plt.savefig(basedir + saving_plot_loc + 'Number_clusters' + well_loc + '.png', dpi=300)
 # plt.show()
 plt.clf()
 
@@ -118,13 +105,13 @@ for p in range(len(time_array)):
 # Plot total 3D
 plt.plot(tot_3D_volume)
 plt.title("Total 3D Volume")
-plt.savefig(basedir + 'clusters/data_analysis/pre-processing/Total_3D_Number_cells_' + well_loc + '.png', dpi=300)
+plt.savefig(basedir + saving_plot_loc + 'Total_3D_Number_cells_' + well_loc + '.png', dpi=300)
 # plt.show()
 plt.clf()
 
 # Plot mean 3D volume of cluster
 plt.plot(mean_3D_volume)
-plt.savefig(basedir + 'clusters/data_analysis/pre-processing/Mean_volume_cluster_' + well_loc + '.png', dpi=300)
+plt.savefig(basedir + saving_plot_loc + 'Mean_volume_cluster_' + well_loc + '.png', dpi=300)
 # plt.show()
 plt.clf()
 
@@ -135,7 +122,7 @@ plt.clf()
 for i in range(len(time_array)):
 
     # Read in area array at current time
-    area_csv_name_list = basedir, exp_type, 'pre_processing_output/', exp_date, '/', well_loc, 't', time_list[i], 'c2', '_area', '.csv'
+    area_csv_name_list = basedir, csv_loc, exp_date, '/', well_loc, 't', time_list[i], 'c2', '_area', '.csv'
     area_csv_name_list_2  =''.join(area_csv_name_list)
     df_slice = pd.read_csv(area_csv_name_list_2, header=None)
     current_array = df_slice.to_numpy()
@@ -147,7 +134,7 @@ for i in range(len(time_array)):
     # plt.imshow(area_slice, cmap=my_cmap, norm=matplotlib.colors.LogNorm(vmin=100,vmax=25000))
     plt.axis([0, current_array.shape[1], 0, current_array.shape[0]])
     plt.colorbar()
-    plt.savefig(f'{basedir + exp_type}images/cluster_sizes_log/frame-{i:03d}.png', bbox_inches='tight', dpi=300)
+    plt.savefig(f'{basedir + saving_plot_loc}gif_frames/frame-{i:03d}.png', bbox_inches='tight', dpi=300)
     plt.clf()
     
 
@@ -160,7 +147,7 @@ images = []
 timestr = time.strftime("%Y%m%d-%H%M%S")
 
 # get all the images in the 'images for gif' folder
-for filename in sorted(glob.glob(basedir + exp_type + 'images/cluster_sizes_log/frame-*.png')): # loop through all png files in the folder
+for filename in sorted(glob.glob(basedir + saving_plot_loc + 'gif_frames/frame-*.png')): # loop through all png files in the folder
     im = Image.open(filename) # open the image
     images.append(im) # add the image to the list
 
@@ -173,7 +160,7 @@ for x in range(0, 9):
     images.append(im)
 
 # save as a gif   
-images[0].save(basedir + exp_type + 'images/cluster_sizes_log/cluster_sizes' + timestr + '.gif',
+images[0].save(basedir + saving_plot_loc + 'gif_' + timestr + '.gif',
             save_all=True, append_images=images[1:], optimize=False, duration=300, loop=0)
 
 ## Code to remove constituent images if not wanting to store
@@ -188,12 +175,12 @@ if plot_hist == True:
     for j in range(len(time_list)):
         plt.hist(cluster_2D_areas[j,:], bins=[0, 1000, 2000, 3000, 4000, 6000, 8000, 10000, 12000, 16000, 20000, 25000])
         plt.ylim(0, 100) 
-        plt.savefig(f'{basedir +  exp_type}images/histogram/frame-{j:03d}.png', bbox_inches='tight', dpi=300)
+        plt.savefig(f'{basedir +  saving_plot_loc}hist_frames/frame-{j:03d}.png', bbox_inches='tight', dpi=300)
         plt.clf()
 
     images_hist = []
     # get all the images in the 'images for gif' folder
-    for filename_hist in sorted(glob.glob(basedir + exp_type + 'images/histogram/frame-*.png')): # loop through all png files in the folder
+    for filename_hist in sorted(glob.glob(basedir + saving_plot_loc + 'hist_frames/frame-*.png')): # loop through all png files in the folder
         im_hist = Image.open(filename_hist) # open the image
         images_hist.append(im_hist) # add the image to the list
 
@@ -206,6 +193,6 @@ if plot_hist == True:
         images_hist.append(im_hist)
 
     # save as a gif   
-    images_hist[0].save(basedir + exp_type + 'images/histogram/' + timestr + '.gif',
+    images_hist[0].save(basedir + saving_plot_loc + 'histogram_' + timestr + '.gif',
                 save_all=True, append_images=images_hist[1:], optimize=False, duration=500, loop=0)
 
