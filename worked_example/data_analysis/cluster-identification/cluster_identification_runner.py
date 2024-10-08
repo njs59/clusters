db1b968dbe1940d488369d44c7ffaa22368098fa
Nathan Schofield <nathan@dhcp-163-1-81-23.maths.ox.ac.uk>
2024-10-08 17:11:15 +0100

    Worked example saving convention updated

--- worked_example/data_analysis/cluster-identification/cluster_identification_runner.py
+++ worked_example/data_analysis/cluster-identification/cluster_identification_runner.py
@@ -3,68 +3,54 @@ import numpy as np
 import pandas as pd
 import time
 
+from osgeo import gdal as GD
+
 from pylab import *
 from scipy.ndimage import *
 
 import read_tif_file_operator as tif
-import pre_pro_operators as pre_oper
+import cluster_identification_operators as iden_oper
 
 t_before = time.time()
 
 
 ###    -----------   Input parameters   --------------     ###
-basedir_data = '/Volumes/Elements/Nate thesis data/Thesis Data/'
-basedir = '/Users/Nathan/Documents/Oxford/DPhil/In_vitro_homogeneous_data/'
+# basedir = '/Users/Nathan/Documents/Oxford/DPhil/In_vitro_homogeneous_data/'
 # experiment = 'RAW_data/2017-02-03_sphere_timelapse/'
-# experiment = 'RAW_data/2017-02-13_sphere_timelapse_2/'
-experiment = ''
 # experiment_data = '2017-02-03 sphere timelapse/'
-# experiment_data = '2017-03-16 sphere TL 6/'
-# experiment_data = '2017-03-10 sphere TL 3pt 4/'
-# experiment_data = '2017-03-13 sphere TL 5/'
-experiment_data = '2017-03-24 sphere 9/'
 # exp_date = '2017-02-03'
-# exp_date = '2017-02-13'
-# exp_date = '2017-03-10'
-# exp_date = '2017-03-13'
-# exp_date = '2017-03-16'
-exp_date = '2017-03-24'
 # folder = 'RAW/Timelapse/sphere_timelapse_useful_wells/'
-folder = ''
 # folder_data = 'RAW/Timelapse/sphere_timelapse/'
-# folder_data = 'RAW/2017-03-16 sphere TL 6/2017-03-13 sphere TL 6-03/'
-# folder_data = 'RAW/2017-03-10 sphere TL 3pt/2017-03-10 sphere TL 3pt/'
-# folder_data = 'RAW/2017-03-13 sphere TL 5/2017-03-13 sphere TL 5/'
-folder_data = 'RAW/2017-03-24 sphere 9/2017-03-24 sphere 9-2/'
+# folder_3 = ''
+# fileID = '.tif'
+# time_array = range(1,98)
+# # Rename single digit values with 0 eg 1 to 01 for consistency
+# time_list = [str(x).zfill(2) for x in time_array]
+# well_loc = 's10'
+# naming_convention_pre_number = basedir, experiment, folder, 'sphere_timelapse_', well_loc, 't'
+# naming_convention_post_number = 'c2', '_ORG', fileID
+# file_saving_folder = '/Users/Nathan/Documents/Oxford/DPhil/clusters/data_analysis_outputs/'
+
+
+basedir = '/Volumes/Elements/Nate thesis data/Thesis Data/'
+experiment = '2017-03-16 sphere TL 6/'
+exp_date = '2017-03-16'
+folder = 'RAW/2017-03-16 sphere TL 6/2017-03-13 sphere TL 6-03/'
 folder_3 = 'sphere_timelapse/'
 fileID = '.tif'
-
-# time_array = range(1,98)
-# time_array = range(1,95)
-# time_array = range(1,143)
-# time_array = range(1,97)
 time_array = range(1,146)
-
 # Rename single digit values with 0 eg 1 to 01 for consistency
 time_list = [str(x).zfill(3) for x in time_array]
-# time_list = [str(x).zfill(3) for x in time_array]
-# time_list= ['21','22','23','24','25','26','27','28','29','30']
-
-# 2017-02-13 sphere timelapse 2_s13t01c2_ORG
-
-# well_loc = 's10'
-# well_loc = 's12'
-# well_loc = 's28'
-
 # well_loc = 's037'
-# well_loc = 's074'
+well_loc = 's074'
+naming_convention_pre_number = basedir, experiment, folder, '2017-03-13 sphere TL 6-03_', well_loc, 't'
+naming_convention_post_number = 'c2', '_ORG', fileID
 
-well_loc = 's10'
-# 4,5,6
+file_saving_folder = '/Users/Nathan/Documents/Oxford/DPhil/clusters/data_analysis_outputs/'
 
 
-# threshold = 320
-# threshold = 440
+
+
 min_clus_size = 150
 use_existing_file = False
 
@@ -88,14 +74,10 @@ def calc_area_arr(arr):
 
 if use_existing_file == False:
     # Convert tif file to 3D array with values between 0 and 1 (1 is maximum intensity point)
-    # raw_arr_3D = tif.tif_to_arr(basedir, experiment, folder, well_loc, time_list, fileID)
-    raw_arr_3D = tif.tif_to_arr(basedir_data, experiment_data, folder_data, well_loc, time_list, fileID)
-
+    raw_arr_3D = tif.tif_to_arr(naming_convention_pre_number, naming_convention_post_number, time_list)
+    
     # Threshold 3D array to boolean array
-    # tf_bool_3D = pre_oper.threshold_arr_supervised(raw_arr_3D, threshold)
-    tf_bool_3D = pre_oper.threshold_arr_unsupervised(raw_arr_3D)
-
-    # print(tf_bool_3D)
+    tf_bool_3D = iden_oper.threshold_arr_unsupervised(raw_arr_3D)
     print(tf_bool_3D.shape)
 
 
@@ -149,7 +131,7 @@ for i in range(len(time_array)):
     # Remove fragments, that is clusters smaller than
     # min_clus_size so we only consider clusters large enough
     # to be a cell rather than a cell fragment
-    area_new, index_keep = pre_oper.remove_fragments(area_list, num_clus, min_clus_size)
+    area_new, index_keep = iden_oper.remove_fragments(area_list, num_clus, min_clus_size)
 
     # Calculate and store total area now in clusters, number of clusters and mean cluster area 
     total_curr_area = np.sum(area_new)
@@ -182,18 +164,18 @@ for i in range(len(time_array)):
 
     # Save area array to csv file
     df_area = pd.DataFrame(area_slice)
-    area_csv_name_list = basedir, 'pre_processing_output/', exp_date, '/', well_loc, 't', time_list[i], 'c2', '_area', '.csv'
+    area_csv_name_list = file_saving_folder, 'csv_files/', exp_date, '/', well_loc, 't', time_list[i], 'c2', '_area', '.csv'
     area_csv_name_list_2  =''.join(area_csv_name_list)
     df_area.to_csv(area_csv_name_list_2, index=False, header=False)
 
     # Save index array to csv file
     df_index = pd.DataFrame(output_label_arr)
-    index_csv_name_list = basedir, 'pre_processing_output/', exp_date, '/', well_loc, 't', time_list[i], 'c2', '_indexed', '.csv'
+    index_csv_name_list = file_saving_folder, 'csv_files/', exp_date, '/', well_loc, 't', time_list[i], 'c2', '_indexed', '.csv'
     index_csv_name_list_2  =''.join(index_csv_name_list)
     df_index.to_csv(index_csv_name_list_2, index=False, header=False)
 
     # Add the cluster areas to 2D array of cluster sizes over time (for histogram plotting)
-    cluster_areas = pre_oper.save_clus_areas(i, area_new, cluster_areas)
+    cluster_areas = iden_oper.save_clus_areas(i, area_new, cluster_areas)
 
 
     t_step_after = time.time()
@@ -216,25 +198,25 @@ print('Shapes', cluster_areas.shape, mean_area.shape, total_area.shape)
 
 # Save 2D cluster areas array to csv
 df_cluster_areas = pd.DataFrame(cluster_areas)
-cluster_areas_csv_name_list = basedir, 'pre_processing_output/', exp_date, '/', well_loc, '_cluster_areas', '.csv'
+cluster_areas_csv_name_list = file_saving_folder, 'csv_files/', exp_date, '/', well_loc, '_cluster_areas', '.csv'
 cluster_areas_csv_name_list_2  =''.join(cluster_areas_csv_name_list)
 df_cluster_areas.to_csv(cluster_areas_csv_name_list_2, index=False, header=False)
 
 # Save mean areas to csv
 df_mean_areas = pd.DataFrame(mean_area)
-mean_areas_csv_name_list = basedir, 'pre_processing_output/', exp_date, '/', well_loc, '_mean_areas', '.csv'
+mean_areas_csv_name_list = file_saving_folder, 'csv_files/', exp_date, '/', well_loc, '_mean_areas', '.csv'
 mean_areas_csv_name_list_2  =''.join(mean_areas_csv_name_list)
 df_mean_areas.to_csv(mean_areas_csv_name_list_2, index=False, header=False)
 
 # Save total areas to csv
 df_total_areas = pd.DataFrame(total_area)
-total_areas_csv_name_list = basedir, 'pre_processing_output/', exp_date, '/', well_loc, '_total_areas', '.csv'
+total_areas_csv_name_list = file_saving_folder, 'csv_files/', exp_date, '/', well_loc, '_total_areas', '.csv'
 total_areas_csv_name_list_2  =''.join(total_areas_csv_name_list)
 df_total_areas.to_csv(total_areas_csv_name_list_2, index=False, header=False)
 
 # Save number of clusters to csv
 df_number_clusters = pd.DataFrame(num_clusters)
-number_clusters_csv_name_list = basedir, 'pre_processing_output/', exp_date, '/', well_loc, '_number_clusters', '.csv'
+number_clusters_csv_name_list = file_saving_folder, 'csv_files/', exp_date, '/', well_loc, '_number_clusters', '.csv'
 number_clusters_csv_name_list_2  =''.join(number_clusters_csv_name_list)
 df_number_clusters.to_csv(number_clusters_csv_name_list_2, index=False, header=False)
     
